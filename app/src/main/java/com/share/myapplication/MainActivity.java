1c467f4717d2701af5a88f9aa7896b3a78f8c91f
pragatijogani <pragatijogani5175@gmail.com>
2021-07-19 15:16:41 -0700

    SECOND

--- app/src/main/java/com/share/myapplication/MainActivity.java
+++ app/src/main/java/com/share/myapplication/MainActivity.java
@@ -14,12 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bg=findViewById(R.id.bg);
-        bg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    
-            }
-        });
+
 
     }
 }
