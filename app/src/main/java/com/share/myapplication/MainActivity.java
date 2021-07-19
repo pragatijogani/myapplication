368ba9ca3c1a1269f6b6b0ddbba93f71c75fa0e1
pragatijogani <pragatijogani5175@gmail.com>
2021-07-19 11:47:17 -0700

    button

--- app/src/main/java/com/share/myapplication/MainActivity.java
+++ app/src/main/java/com/share/myapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.share.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
