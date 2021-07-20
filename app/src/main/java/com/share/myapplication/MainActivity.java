4c1c04a8415738972f30cdcdd54ed052e18f23a0
khushbu kachchhi <khushbukachhi000@gmail.com>
2021-07-19 23:56:25 -0700

    button123

--- app/src/main/java/com/share/myapplication/MainActivity.java
+++ app/src/main/java/com/share/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.share.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bg=findViewById(R.id.bg);
+        bg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
