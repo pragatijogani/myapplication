55fb65213a60a8620204ebf5e80353177ac9d911
khushbu kachchhi <khushbukachhi000@gmail.com>
2021-07-19 23:39:20 -0700

    button added

--- app/src/main/java/com/share/myapplication/MainActivity.java
+++ app/src/main/java/com/share/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
